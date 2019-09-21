42aa68b83413f5e50f739307044557cac50f9327
asadalibhatti <i170312@nu.edu.pk>
2019-09-21 23:26:50 +0500

    c2

--- p2/src/q5.java
+++ p2/src/q5.java
@@ -6,33 +6,50 @@ public class q5 {
             throws IOException {
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        //System.out.println("Enter User1 : ");
-        String user1 ="Arshadbhatti";// myObj.nextLine();  // Read user input
+        System.out.println("Enter User1 : ");
+        String user1 = myObj.nextLine();  // Read user input
         BufferedWriter writer = new BufferedWriter(new FileWriter("user1.txt"));
         writer.write(user1);
+        writer.close();
 
-        //System.out.println("Enter User2 : ");
-        String user2 = "asadali";//myObj.nextLine();  // Read user input
+        System.out.println("Enter User2 : ");
+        String user2 = myObj.nextLine();  // Read user input
         writer = new BufferedWriter(new FileWriter("user2.txt"));
         writer.write(user2);
-
         writer.close();
 
 
         File file1 = new File("user1.txt");
         BufferedReader br = new BufferedReader(new FileReader(file1));
-        String st;
-        while ((st = br.readLine()) != null)
-            System.out.println(st);
+        String st,st2;
+
 
         File file2 = new File("user2.txt");
         BufferedReader br2 = new BufferedReader(new FileReader(file2));
 
 
 
-        System.out.println("\n user2 file:\n");
-        while ((st = br2.readLine()) != null)
+
+        while (true) {
+            st = br2.readLine();
+            st2 = br.readLine();
+            if(st == null || st2==null){
+                if (st2 == st) {
+                    System.out.println("Matched");
+                    break;
+                }
+                System.out.println(" not Matched");
+            }
+
+
+            if (st2 != st) {
+                System.out.println("not Matched");
+                break;
+
+            }
+
             System.out.println(st);
+        }
     }
 
     public static void main(String []args) {
